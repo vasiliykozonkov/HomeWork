87e14a6e0e7dd9543f03e7e6d1ba7d31650e34cd
Vasiliy Kozonkov <vasiliykozonkov@gmail.com>
2026-08-25 10:04:47 +0000

    Привел к одному пайплайну и добавил вывод в консоль сортированные книги по количеству страниц и году

--- 2.LibraryStreamProcessor/src/main/java/LibraryApp.java
+++ 2.LibraryStreamProcessor/src/main/java/LibraryApp.java
@@ -1,5 +1,4 @@
 import java.util.List;
-import java.util.Optional;
 
 public class LibraryApp {
 	public static void main(String[] args) {
@@ -8,14 +7,6 @@ public class LibraryApp {
 
 		List<Student> students = readerService.readStudents();
 
-		processor.printStudentsAndBooks(students);
-
-		Optional<Integer> resultYear = processor.findFirstBookYear(students);
-
-		System.out.println("\n=== РЕЗУЛЬТАТ ===");
-		resultYear.ifPresentOrElse(
-				year -> System.out.println("Год выпуска найденной книги: " + year),
-				() -> System.out.println("Такая книга отсутствует")
-		);
+		processor.processLibrary(students);
 	}
 }
--- 2.LibraryStreamProcessor/src/main/java/LibraryProcessor.java
+++ 2.LibraryStreamProcessor/src/main/java/LibraryProcessor.java
@@ -1,30 +1,31 @@
+import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
 public class LibraryProcessor {
 
-    private static final int MIN_YEAR = 2000;
-    private static final int BOOKS_LIMIT = 3;
+	private static final int MIN_YEAR = 2000;
+	private static final int BOOKS_LIMIT = 3;
 
-    public void printStudentsAndBooks(List<Student> students) {
-        students.stream()
-                .flatMap(student -> student.getBooks().stream()
-                        .sorted((b1, b2) -> Integer.compare(b1.getPages(), b2.getPages()))
-                        .distinct()
-                        .filter(book -> book.getYear() > MIN_YEAR)
-                        .map(book -> student + " → Книга: " + book))
-                .forEach(System.out::println);
-    }
-
-    public Optional<Integer> findFirstBookYear(List<Student> students) {
-        return students.stream()
-                .flatMap(student -> student.getBooks().stream())
-                .filter(book -> book.getYear() > MIN_YEAR)
-                .distinct()
-                .sorted((b1, b2) -> Integer.compare(b1.getPages(), b2.getPages()))
-                .limit(BOOKS_LIMIT)
-                .map(Book::getYear)
-                .findFirst();
-    }
-
+	public void processLibrary(List<Student> students) {
+		students.stream()
+		.peek(student -> {
+			System.out.println(student);
+			student.getBooks().stream()
+			.sorted(Comparator.comparingInt(Book::getPages))
+			.filter(book -> book.getYear() > MIN_YEAR)
+			.forEach(book -> System.out.println("   📚 " + book));
+		})
+		.map(Student::getBooks)
+		.flatMap(java.util.Collection::stream)
+		.sorted(Comparator.comparingInt(Book::getPages))
+		.distinct()
+		.filter(book -> book.getYear() > MIN_YEAR)
+		.limit(BOOKS_LIMIT)
+		.map(Book::getYear)
+		.findFirst()
+		.ifPresentOrElse(
+			year -> System.out.println("\nГод выпуска найденной книги: " + year),
+			() -> System.out.println("Такая книга отсутствует")
+		);
+	}
 }
